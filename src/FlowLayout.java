a288663f67e094c3a35f5cbbc25769281adca877
Varun-Babu <itsme.varun.26@gmail.com>
2022-12-29 19:41:47 +0530

    flowlayout

--- src/FlowLayout.java
+++ src/FlowLayout.java
@@ -1,10 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class FlowLayout extends JFrame  {
+    FlowLayout(){
 
-public class FlowLayout  {
-
+        JButton bt1 = new JButton("Yes");
+        JButton bt2 = new JButton("No");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new java.awt.FlowLayout());
+        setSize(400,400);
+        add(bt1);
+        add(bt2);
+        setVisible(true);
+    }
 
     public static void main(String[] args) {
 
+        new FlowLayout();
 
     }
 }
